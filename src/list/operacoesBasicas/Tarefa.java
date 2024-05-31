6d17039425d7013a994c58f2986ecc191ecd116a
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-05-31 00:04:29 -0300

    Criando métodos da lista principal

---
+++ src/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package list.operacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
